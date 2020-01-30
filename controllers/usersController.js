446f00e0debfad8fe061a5c052bae5538beac1af
meke101 <ameliacaffrey@outlook.com>
2020-01-30 16:37:39 +0000

    users controller skeleton commit

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,22 +1,40 @@
 const {
-  insertUser
-  // updateArticle,
-  // insertComment,
-  // fetchComments,
-  // fetchAllArticles
-} = require("../models/articlesModel");
-
+  insertUser,
+  fetchUser,
+  updateUser,
+  insertUserTags
+} = require("../models/usersModel");
 
 exports.postUser = (req, res, next) => {
-  insertUser(req.params._id)
-    .then(([article]) => res.status(200).send({ article: article }))
+  insertUser(req.body)
+    .then(([user]) => res.status(201).send({ user: user }))
     .catch(next);
 };
 
-exports.getUser = (req, res, next) => {};
+exports.getUser = (req, res, next) => {
+  fetchUser(req.params.user_id)
+    .then(([user]) => res.status(200).send({ user: user }))
+    .catch(next);
+};
 
-exports.patchUser = (req, res, next) => {};
+exports.patchUser = (req, res, next) => {
+  const { user_id } = req.params;
+  updateUser(user_id, req.body)
+    .then(([user]) => res.status(200).send({ user }))
+    .catch(next);
+};
 
-exports.postTags = (req, res, next) => {};
+exports.postTags = (req, res, next) => {
+   const { user_id } = req.params;
 
-exports.patchTags = (req, res, next) => {};
+  insertUserTags(user_id, req.body)
+    .then(tags => res.status(201).send({ tags: tags }))
+    .catch(next);
+};
+
+exports.patchTags = (req, res, next) => {
+    const { user_id } = req.params;
+    updateUserTags(user_id, req.body)
+      .then(([user]) => res.status(200).send({ user }))
+      .catch(next);
+};
