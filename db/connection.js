116442e6da9c8ce11797290693aef12920ed6283
meke101 <ameliacaffrey@outlook.com>
2020-01-31 12:15:48 +0000

    connection.js done

---
+++ db/connection.js
@@ -0,0 +1,10 @@
+//linking current config db and knex
+const ENV = process.env.NODE_ENV || "development";
+const knex = require("knex");
+
+const dbConfig =
+  ENV === "production"
+    ? { client: "pg", connection: process.env.DATABASE_URL }
+    : require("../knexfile");
+
+module.exports = knex(dbConfig);
