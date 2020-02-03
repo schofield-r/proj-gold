551607e33da30a8f62e9a8040d4914c510449fc8
Lewis Walwyn <lewiswalwyn@Lewiss-MacBook-Pro.local>
2020-02-03 12:37:30 +0000

    boot strap broke carousel

--- pg-frontend/src/app/app.module.ts
+++ pg-frontend/src/app/app.module.ts
@@ -4,11 +4,19 @@ import { NgModule } from "@angular/core";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { HeaderComponent } from './components/header/header.component';
-import { NavDropdownComponent } from './components/bootstrap/nav-dropdown/nav-dropdown.component';
+import { HeaderComponent } from "./components/header/header.component";
+import { NavDropdownComponent } from "./components/bootstrap/nav-dropdown/nav-dropdown.component";
+import { HomepageComponent } from "./components/pages/homepage/homepage.component";
+import { HomepageCarouselComponent } from "./components/bootstrap/homepage-carousel/homepage-carousel.component";
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, NavDropdownComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    NavDropdownComponent,
+    HomepageComponent,
+    HomepageCarouselComponent
+  ],
   imports: [BrowserModule, AppRoutingModule, NgbModule],
   providers: [],
   bootstrap: [AppComponent]
