be093485e02722a76f75c23d1280d5e4aa9d8eb6
justJackjon <github@redrobincreative.com>
2020-02-03 16:42:17 +0000

    Add routing

--- pg-frontend/src/app/app-routing.module.ts
+++ pg-frontend/src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
+import { HomepageComponent } from "./components/pages/homepage/homepage.component";
+import { ProjectOverviewComponent } from "./components/pages/project-overview/project-overview.component";
+import { ProjectProgressComponent } from "./components/pages/project-progress/project-progress.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "project-overview", component: ProjectOverviewComponent },
+  { path: "project-progress", component: ProjectProgressComponent },
+  { path: "", component: HomepageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- pg-frontend/src/app/app.module.ts
+++ pg-frontend/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { NavDropdownComponent } from "./components/bootstrap/nav-dropdown/nav-dr
 import { HomepageComponent } from "./components/pages/homepage/homepage.component";
 import { HomepageCarouselComponent } from "./components/bootstrap/homepage-carousel/homepage-carousel.component";
 import { ProjectOverviewComponent } from "./components/pages/project-overview/project-overview.component";
+import { ProjectProgressComponent } from './components/pages/project-progress/project-progress.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import { ProjectOverviewComponent } from "./components/pages/project-overview/pr
     NavDropdownComponent,
     HomepageComponent,
     HomepageCarouselComponent,
-    ProjectOverviewComponent
+    ProjectOverviewComponent,
+    ProjectProgressComponent
   ],
   imports: [BrowserModule, AppRoutingModule, NgbModule],
   providers: [],
