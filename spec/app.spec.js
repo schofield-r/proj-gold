f6fc76fc4d9513c4acdbe5350ddc47e3d5f4a504
schofield-r <yenschofield@gmail.com>
2020-02-04 10:22:17 +0000

    add happy path tests for front end integration

--- spec/app.spec.js
+++ spec/app.spec.js
@@ -57,6 +57,17 @@ describe("/api", () => {
         .expect(200)
         .then(res => {
           expect(res.body.project).to.be.an("object");
+          expect(res.body.project).to.eql({
+            project_id: 1,
+            created_by: "kirkley",
+            project_leader: "HireUs?",
+            status: "Testing",
+            votes: 56,
+            description:
+              "ullamco reprehenderit deserunt ipsum ullamco pariatur est magna nulla ea excepteur labore labore sunt culpa tempor ullamco amet velit duis",
+            project_title: "HIVEDOM",
+            date_posted: 1163852514171
+          });
         });
     });
   });
@@ -113,14 +124,11 @@ describe("/api", () => {
     });
   });
   describe("DELETE /projects/:project_id/tags", () => {
-    it("200 response, deletes a tag from the projects_tag table", () => {
+    it("204 response, deletes a tag from the projects_tag table", () => {
       return request(app)
         .delete("/api/projects/4/tags")
         .send({ tag_id: 6 })
-        .expect(200)
-        .then(res => {
-          expect(res.body.msg).to.equal("tag deleted");
-        });
+        .expect(204);
     });
   });
   describe("POST /projects/:project_id/collaborate", () => {
@@ -134,15 +142,164 @@ describe("/api", () => {
         });
     });
   });
-  describe("GET /projects/:project_id/comments", () => {});
-  describe("POST /projects/:project_id/comments", () => {});
-  describe("DELETE /comments/:comment_id", () => {});
-  describe("GET /comments/:comment_id", () => {});
-  describe("POST /users", () => {});
-  describe("GET /users/:username", () => {});
-  describe("DELETE /users/:username", () => {});
-  describe("POST /users/:username/update_preferences", () => {});
-  describe("DELETE /users/:username/update_preferences", () => {});
-  describe("POST /users/:username/update_tags", () => {});
-  describe("DELETE /users/:username/update_tags", () => {});
+  describe("GET /projects/:project_id/comments", () => {
+    it("200 response, gets a projects comments", () => {
+      return request(app)
+        .get("/api/projects/1/comments")
+        .expect(200)
+        .then(res => {
+          expect(res.body.comments).to.be.an("array");
+          expect(res.body.comments[0]).to.contain.keys(
+            "project_id",
+            "comment_id",
+            "created_by",
+            "body",
+            "date_created"
+          );
+        });
+    });
+  });
+  describe("POST /projects/:project_id/comments", () => {
+    it("201 response, ", () => {
+      return request(app)
+        .post("/api/projects/1/comments")
+        .send({ created_by: dave, body: "this is a comment" })
+        .expect(204)
+        .then(res => {
+          expect(res.body.comment).to.contain.keys(
+            "comment_id",
+            "project_id",
+            "created_by",
+            "date_created",
+            "body"
+          );
+          expect(res.body.comment.created_by).to.equal("dave");
+        });
+    });
+  });
+  describe("DELETE /comments/:comment_id", () => {
+    it("204 response, ", () => {
+      return request(app)
+        .delete("/api/comments/1")
+        .expect(204);
+    });
+  });
+  describe("GET /comments/:comment_id", () => {
+    it("200 response,", () => {
+      return request(app)
+        .get("/api/comments/1")
+        .expect(200)
+        .then(res => {
+          expect(res.body.comment).to.eql({
+            project_id: 1,
+            created_by: "Goodbill",
+            body:
+              "do ipsum pariatur excepteur id commodo quis irure aliqua deserunt fugiat et",
+            date_created: "2019-12-28T06:56:34 -00:00"
+          });
+        });
+    });
+  });
+  describe("POST /users", () => {
+    it("201 response,", () => {
+      return request(app)
+        .post("/api/users")
+        .send({
+          first_name: "ralf",
+          surname: "markson",
+          location: "Liverpool",
+          loc_radius: 0,
+          description:
+            "Lorem labore proident nisi laboris enim Lorem adipisicing eiusmod consectetur aliqua eu cillum exercitation dolore ut",
+          username: "ZeroCool26",
+          email: "youington@acusage.com",
+          github: "Harrin",
+          digest_opt_in: false
+        })
+        .expect(201)
+        .then(res => {
+          expect(res.body.user).to.eql({
+            first_name: "ralf",
+            surname: "markson",
+            location: "Liverpool",
+            loc_radius: 0,
+            description:
+              "Lorem labore proident nisi laboris enim Lorem adipisicing eiusmod consectetur aliqua eu cillum exercitation dolore ut",
+            username: "ZeroCool26",
+            email: "youington@acusage.com",
+            github: "Harrin",
+            digest_opt_in: false
+          });
+        });
+    });
+  });
+  describe("GET /users/:username", () => {
+    it("200 response,", () => {
+      return request(app)
+        .get("/api/users/CookieCoder")
+        .expect(200)
+        .then(res => {
+          expect(res.body.user).to.eql({
+            first_name: "Dixon",
+            surname: "Howell",
+            location: "Birmingham",
+            loc_radius: 20,
+            description:
+              "dolore esse nulla fugiat quis sunt qui voluptate pariatur laborum Lorem Lorem adipisicing aliqua ex aliquip",
+            username: "CookieCoder",
+            email: "dixonhowell@acusage.com",
+            github: "Howell20",
+            digest_opt_in: false
+          });
+        });
+    });
+  });
+  describe("DELETE /users/:username", () => {
+    it("204 response,", () => {
+      return request(app)
+        .delete("/api/users/CookieCoder")
+        .expect(204);
+    });
+  });
+  describe("POST /users/:username/update_preferences", () => {
+    it("201 response,", () => {
+      return request(app)
+        .post("/api/users/dave/")
+        .send({ role_id: 3 })
+        .expect(201)
+        .then(res => {
+          expect(res.body.userRole).to.equal({
+            username: "dave",
+            user_role: 3
+          });
+        });
+    });
+  });
+  describe("DELETE /users/:username/update_preferences", () => {
+    it("204 response,", () => {
+      return request(app)
+        .delete("/api/users/dave/")
+        .send({ role_id: 4 })
+        .expect(204);
+    });
+  });
+  describe("POST /users/:username/update_tags", () => {
+    it("201 response,", () => {
+      return request(app)
+        .post("/api/users/dave/update_tags")
+        .send({ tag_id: 18 })
+        .expect(201)
+        .then(res => {
+          expect(res.body.tag).to.equal({ username: "dave", tag_id: 18 });
+        });
+    });
+  });
+  describe("DELETE /users/:username/update_tags", () => {
+    it("204 response,", () => {
+      return request(app)
+        .delete("/api/users/dave/update_tags")
+        .send({ tag_id: 21 })
+        .expect(204);
+    });
+  });
 });
