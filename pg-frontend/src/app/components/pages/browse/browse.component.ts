0b837fad76c89abe666a87df5d8e7fa27dd0003d
Lewis Walwyn <lewiswalwyn@Lewiss-MacBook-Pro.local>
2020-02-05 16:35:32 +0000

    ready for margarine

--- pg-frontend/src/app/components/pages/browse/browse.component.ts
+++ pg-frontend/src/app/components/pages/browse/browse.component.ts
@@ -1,15 +1,68 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { ProjectCardComponent } from "../../cards/project-card/project-card.component";
 
 @Component({
-  selector: 'app-browse',
-  templateUrl: './browse.component.html',
-  styleUrls: ['./browse.component.css']
+  selector: "app-browse",
+  templateUrl: "./browse.component.html",
+  styleUrls: ["./browse.component.css"]
 })
 export class BrowseComponent implements OnInit {
+  selected: string;
+  displayData: Array<object> = [];
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private http: HttpClient) {
+    this.selected = "something";
   }
 
+  ngOnInit() {
+    // this.http
+    //   .get<any>("https://project-gold-api.herokuapp.com/api/projects")
+    //   .subscribe(
+    //     data => console.log(data.projects),
+    //     error => console.error("There was an error!", error)
+    //   );
+  }
+
+  selectorClicked(whichButton) {
+    this.selected = whichButton;
+    console.log(whichButton);
+
+    if (whichButton === "projects")
+      this.http
+        .get<any>(`https://project-gold-api.herokuapp.com/api/projects`)
+        .subscribe(
+          data =>
+            data.projects.map(project => {
+              if (project.status !== "Pitch") {
+                console.log(
+                  "Title: ",
+                  project.project_title,
+                  " Status: ",
+                  project.status
+                );
+              }
+            }),
+          error => console.error("There was an error!", error)
+        );
+
+    if (whichButton === "pitches")
+      this.http
+        .get<any>(`https://project-gold-api.herokuapp.com/api/projects`)
+        .subscribe(
+          data =>
+            data.projects.map(project => {
+              if (project.status === "Pitch") {
+                // console.log(
+                //   "Title: ",
+                //   project.project_title,
+                //   " Status: ",
+                //   project.status
+                this.displayData.push(project);
+                console.log(this.displayData);
+              }
+            }),
+          error => console.error("There was an error!", error)
+        );
+  }
 }
