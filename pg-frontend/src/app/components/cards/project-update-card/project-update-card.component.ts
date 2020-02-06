fa536206da9da28b817eea20c38555850cbc7f74
justJackjon <github@redrobincreative.com>
2020-02-06 12:05:50 +0000

    Adjust project page styling

--- pg-frontend/src/app/components/cards/project-update-card/project-update-card.component.ts
+++ pg-frontend/src/app/components/cards/project-update-card/project-update-card.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from "@angular/core";
 
 @Component({
-  selector: 'app-project-update-card',
-  templateUrl: './project-update-card.component.html',
-  styleUrls: ['./project-update-card.component.css']
+  selector: "app-project-update-card",
+  templateUrl: "./project-update-card.component.html",
+  styleUrls: ["./project-update-card.component.css"]
 })
 export class ProjectUpdateCardComponent implements OnInit {
-
-  constructor() { }
+  @Input() comment: any;
+  date: string = "";
+  image = `https://i.pravatar.cc/80?${Math.floor(Math.random() * 70)}`;
+  constructor() {}
+  //
 
   ngOnInit() {
+    this.date = new Date(this.comment.date_created).toUTCString();
   }
 
+  ngOnChange() {
+    this.date = this.comment.date_created;
+  }
 }
